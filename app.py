4d9e012e50bee2cf13ba5eb90b2b175620615ea8
Simon Wang <lovesummer718@gmail.com>
2023-08-07 21:18:31 +0800

    Getting started

---
+++ app.py
@@ -0,0 +1,31 @@
+# Getting Started
+# First program
+print("Hello World!")
+
+# pylinting
+# PEP8
+# CPython Jython IronPython PyPy
+x = 1
+print("*" * 10)
+
+# student_count = 1_000
+# rating = 4.99
+# is_published = True
+# course_name = "Python"
+# print(student_count)
+
+# length = len(course_name)
+# print(length)
+
+# first_name = 'John'
+# last_name = 'Wang'
+# full_name = f"{first_name} {last_name}"
+# print(full_name)
+
+
+temperature = 35
+if temperature < 40:
+    print('It is warm')
+    print(temperature)
+
+print('It is cold')
