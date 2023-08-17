6faba22318e69d402720940cb18efa2d8c83360b
Simon Wang <lovesummer718@gmail.com>
2023-08-17 19:57:30 +0800

    first commit

--- app.py
+++ app.py
@@ -1,21 +1,41 @@
 # Getting Started
 # First program
-print("Hello World!")
+# print("Hello World!")
 
 # pylinting
 # PEP8
 # CPython Jython IronPython PyPy
-x = 1
-print("*" * 10)
+# x = 1
+# print("*" * 10)
 
-# student_count = 1_000
-# rating = 4.99
-# is_published = True
+
+# Primitive types
+# variables
+
+# student_count = 1_000  # integer
+# print(student_count, "students")
+# rating = 4.99  # float
+# is_published = True  # boolean
 # course_name = "Python"
 # print(student_count)
 
+# string types
 # length = len(course_name)
 # print(length)
+# print(course_name[0])
+# print(course_name[-1])
+# print(course_name[:4])
+
+# Escape sequences
+
+import math
+
+
+course = "Python \'programming"
+# print(course)
+
+
+# formatted string
 
 # first_name = 'John'
 # last_name = 'Wang'
@@ -23,9 +43,45 @@ print("*" * 10)
 # print(full_name)
 
 
-temperature = 35
-if temperature < 40:
-    print('It is warm')
-    print(temperature)
+# string methods
 
-print('It is cold')
+# print(course.upper())
+# print(course.lower())
+# print(course.rstrip())
+# print("Python" in course)
+
+# numbers: integers, floats, complex numbers
+
+# print(10 + 5)
+# print(10 - 5)
+# print(10 / 5)
+# print(10 * 5)
+# print(10 // 5)
+# print(10 % 5)
+# print(10 ** 5)
+
+# work with numbers
+
+# print(round(10.4))
+# print(abs(-3.5))
+# print(math.sqrt(16))
+
+# type conversions
+
+# x = int(input("x: "))
+# y = x + 1
+
+# int()
+# float()
+# bool()
+# str()
+
+# print(type(y))
+
+
+# temperature = 35
+# if temperature < 40:
+#     print('It is warm')
+#     print(temperature)
+
+# print('It is cold')
